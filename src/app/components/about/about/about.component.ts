4072183e043e8a4da8753cf1a2bcc6c3186265ec
KevinJG994 <kevinJG994@gmail.com>
2024-04-01 18:32:03 +0100

    Navegador funcionando

--- src/app/components/about/about/about.component.ts
+++ src/app/components/about/about/about.component.ts
@@ -9,15 +9,15 @@ export class AboutComponent {
   links: any [] = [
     {
       imagen: "https://github.com/KevinJG994/KevinJG994/assets/96546093/c0265393-a350-4ce5-860c-aec343f9f781",
-      URL: "https://www.linkedin.com/in/kevin-jim%C3%A9nez94/"
+      URL: "#"
     },
     {
       imagen: "https://github.com/KevinJG994/KevinJG994/assets/96546093/734d10d0-eaa8-420d-b3de-b0d4e948986c",
-      URL: "https://github.com/KevinJG994/"
+      URL: "#"
     },
     {
       imagen: "https://github.com/KevinJG994/KevinJG994/assets/96546093/e822eade-58d2-4800-abd1-1970b6a340a2",
-      URL: "mailto:kevinjg994@gmail.com"
+      URL: "#"
     },
   ]
 
--- src/app/components/proyects/proyects/proyects.component.ts
+++ src/app/components/proyects/proyects/proyects.component.ts
@@ -11,22 +11,22 @@ export class ProyectsComponent {
     {
       name: 'Proyect1',
       githubURL: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      imagen: 'https://github.com/KevinJG994/KevinJG994/assets/96546093/b11e1caf-fea6-4409-8074-175d2f8198ee'
+      imagen: 'https://pandorafms.com/blog/wp-content/uploads/2018/10/Breve-Historia-de-la-Gestion-de-Proyectos.jpg'
     },
     {
       name: 'Proyect2',
       githubURL: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      imagen: "https://github.com/KevinJG994/KevinJG994/assets/96546093/c0265393-a350-4ce5-860c-aec343f9f781"
+      imagen: "https://pandorafms.com/blog/wp-content/uploads/2018/10/Breve-Historia-de-la-Gestion-de-Proyectos.jpg"
     },
     {
       name: 'Proyect3',
       githubURL: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      imagen: 'https://github.com/KevinJG994/KevinJG994/assets/96546093/734d10d0-eaa8-420d-b3de-b0d4e948986c'
+      imagen: 'https://empresa.org.ar/wp-content/uploads/2019/01/gestion-de-proyectos-1.jpeg'
     },
     {
-      name: 'Proyect3',
+      name: 'Proyect4',
       githubURL: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      imagen: 'https://github.com/KevinJG994/KevinJG994/assets/96546093/b11e1caf-fea6-4409-8074-175d2f8198ee'
+      imagen: 'https://empresa.org.ar/wp-content/uploads/2019/01/gestion-de-proyectos-1.jpeg'
     },
   ]
 }
